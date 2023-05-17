32d1cd53f56871cf50b9d446e68d52f8fce09a87
PaulEllis2001 <paul@pdellis.com>
2023-05-17 17:17:08 -0600

    Added About page and temp file for resume

--- about.php
+++ about.php
@@ -1,3 +1,18 @@
 <?php include('includes/header.php'); ?>
-
+<h1> Contact Me </h1>
+<p>
+    Email: <a href="mailto:paul@pdellis.com">paul@pdellis.com</a>
+    <br>
+    Phone Number: (408) 963-9091
+    <br>
+    All emails and phone calls will be responded to at my earliest convenience. If you do not hear back within 2 business days please try again.
+</p>
+<h1> Resume </h1>
+<p>
+    You can download a PDF version of my Resume <a href="files/resume.pdf">here</a>.
+</p>
+<h1> GitHub </h1>
+<p>
+    You can find my GitHub profile <a href="https://github.com/PaulEllis2001">here</a>.
+</p>
 <?php include('includes/footer.php'); ?>
